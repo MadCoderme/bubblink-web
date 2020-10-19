d1913e40337654798700831bed1365ffe744caf2
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2020-10-19 14:23:44 +0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -25,7 +25,7 @@ messaging.setBackgroundMessageHandler(function(payload) {
                }
           })
           .then(() => {
-               return registration.showNotification("my notification title");
+               return registration.showNotification("New Message in Bubblink");
           });
      return promiseChain;
 });
